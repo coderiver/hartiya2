61167f4be9bc6ac9bd62df62387a6554d712e4da
buschidos <buschidos@buschidos.lan>
2014-08-13 21:38:45 +0300

    topnews tabs

--- js/common.js
+++ js/common.js
@@ -1,8 +1,24 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	var tabs        = $('.js-tabs'),
+		tab         = tabs.find('.js-tab').children(),
+		tabContent  = tabs.find('.js-tabcontent').children(),
+		activeClass = 'is-active';
 
-	console.log($('body').html());
+	// hide all tabcontents exept first
+	tabContent.not(':first-child').hide();
+
+	tab.click(function(event) {
+		var activeTabIndex = $(this).index();
+		// change active tab
+		tab.removeClass(activeClass);
+		$(this).addClass(activeClass);
+		// change active tabcontent
+		tabContent.hide();
+		tabContent.eq(activeTabIndex).show();
+		return false;
+	});
+
 });
+
+
