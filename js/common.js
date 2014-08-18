27106744b971430265c327bef1562dd9c950e682
buschidos <buschidos@buschidos.local>
2014-08-18 20:01:11 +0300

    article

--- js/common.js
+++ js/common.js
@@ -35,6 +35,18 @@ head.ready(function() {
 		return false;
 	});
 
+	//articles galleries synchronize
+	var slideshows = $('.cycle-slideshow').on('cycle-next cycle-prev', function(e, opts) {
+		slideshows.not(this).cycle('goto', opts.currSlide);
+	});
+
+	$('.gallery__pager .slide').click(function() {
+		var index = $('.gallery__pager').data('cycle.API').getSlideIndex(this);
+		console.log(index);
+		slideshows.cycle('goto', index);
+	});
+
+
 });
 
 
