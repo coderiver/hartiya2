72edcc8390e090ae80fea6781a2bcc51cd252cf8
buschidos <sergey.snozyk@gmail.com>
2014-10-28 16:03:41 +0200

    added comments, fix bug with sidebar when scroll

--- js/common.js
+++ js/common.js
@@ -56,85 +56,85 @@ head.ready(function() {
 	});
 
 	//change position of sidebar to fixed/to static when scrolling document
-	var changeSidebarPosition = function() {
 
-		$(document).scroll(function(event) {
-			b = $('body');
-			if($(document).scrollTop()>400){
-				b.addClass('can-gototop')
-			} else{
-				b.removeClass('can-gototop')
+	$(document).scroll(function(event) {
+		b = $('body');
+		if($(document).scrollTop()>400){
+			b.addClass('can-gototop')
+		} else{
+			b.removeClass('can-gototop')
+		}
+
+		var sidebarBottomPosition = wrapper.offset().top + sidebar.height(),
+			wrapperTopPosition    = wrapper.offset().top,
+			wrapperBottomPosition = wrapper.offset().top + wrapper.height();
+
+		// check if layout is
+		if ( wrapper.parents('.l_article').length !== 0 && $(window).width() < 1150
+			|| $('.l-main').height() < sidebar.height() ) {
+			return false;
+		};
+
+		if (wrapper.children('.l-top').length !== 0 && $(window).width() < 1150) {
+			sidebarBottomPosition = wrapper.offset().top + $('.l-top').height() + 50 + sidebar.height(),
+			wrapperTopPosition    = wrapper.offset().top + $('.l-top').height() + 50;
+		};
+
+		// when sidebar height is less than window height
+		if ( sidebar.height() < $(window).height() ) {
+			var targetTopPosition    = $(document).scrollTop() + 20,
+				targetBottomPosition = $(document).scrollTop() + sidebar.height() +20;
+
+			if ( targetTopPosition >= wrapperTopPosition ) {
+				sidebar.css({
+					position: 'fixed',
+					top: '20px',
+					bottom: 'auto'
+				});
 			}
-
-			var sidebarBottomPosition = wrapper.offset().top + sidebar.height(),
-				wrapperTopPosition    = wrapper.offset().top,
-				wrapperBottomPosition = wrapper.offset().top + wrapper.height();
-
-			// check if layout is
-			if ( wrapper.parents('l-article').length == 0 && $(window).width() < 1150 ) {
-				return false;
-			};
-
-			// when sidebar height is less than window height
-			if ( sidebar.height() < $(window).height() ) {
-				var targetTopPosition    = $(document).scrollTop() + 20,
-					targetBottomPosition = $(document).scrollTop() + sidebar.height() +20;
-
-				if ( targetTopPosition >= wrapperTopPosition ) {
-					sidebar.css({
-						position: 'fixed',
-						top: '20px',
-						bottom: 'auto'
-					});
-				}
-				else {
-					sidebar.css({
-						position: 'static',
-						top: 'auto',
-						bottom: 'auto'
-					});
-				};
-
-				if ( targetBottomPosition >= wrapperBottomPosition ) {
-					sidebar.css({
-						position: 'absolute',
-						top: 'auto',
-						bottom: '0'
-					});
-				};
-			}
-			// when sidebar height is larger than window height
 			else {
-				var targetTopPosition    = $(document).scrollTop() + $(window).height();
-
-				if ( targetTopPosition >= sidebarBottomPosition ) {
-					sidebar.css({
-						position: 'fixed',
-						top: 'auto',
-						bottom: '0'
-					});
-				}
-				else {
-					sidebar.css({
-						position: 'static',
-						top: 'auto',
-						bottom: 'auto'
-					});
-				};
-
-				if ( targetTopPosition >= wrapperBottomPosition ) {
-					sidebar.css({
-						position: 'absolute',
-						top: 'auto',
-						bottom: '0'
-					});
-				};
+				sidebar.css({
+					position: 'static',
+					top: 'auto',
+					bottom: 'auto'
+				});
 			};
-		});
-	};
 
-	// if ( !sidebar.hasClass('js-no-change') ) {
-		changeSidebarPosition();
-	// };
+			if ( targetBottomPosition >= wrapperBottomPosition ) {
+				sidebar.css({
+					position: 'absolute',
+					top: 'auto',
+					bottom: '0'
+				});
+			};
+		}
+		// when sidebar height is larger than window height
+		else {
+			var targetTopPosition    = $(document).scrollTop() + $(window).height();
+
+			if ( targetTopPosition >= sidebarBottomPosition ) {
+				sidebar.css({
+					position: 'fixed',
+					top: 'auto',
+					bottom: '0'
+				});
+			}
+			else {
+				sidebar.css({
+					position: 'static',
+					top: 'auto',
+					bottom: 'auto'
+				});
+			};
+
+			if ( targetTopPosition >= wrapperBottomPosition ) {
+				sidebar.css({
+					position: 'absolute',
+					top: 'auto',
+					bottom: '0'
+				});
+			};
+		};
+	});
 
 });
